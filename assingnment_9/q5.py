66fd1e39a6c3f55263a0196e3c96604c5c4a1007
Stefan Bjarni Hjaltested <stefanh@Stefans-MacBook-Pro.local>
2018-09-20 20:33:03 +0000

    allt nema dori

--- assingnment_9/q5.py
+++ assingnment_9/q5.py
@@ -2,9 +2,9 @@ with open("words.txt", "r") as words_file:
     with open("sentences.txt", "w") as sentences_file:
         for line_str in words_file:
             if line_str == "\n":
-                print(line_str,file=sentence_file,end="")
+                print(line_str, file = sentences_file, end="")
                 print(line_str, end="")
             else:
                 line_str = line_str.strip()
-                print(line_str,file=sentence_file,end=" ")
+                print(line_str, file =sentences_file, end=" ")
                 print(line_str,end=" ")
