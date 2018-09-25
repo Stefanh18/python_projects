32ce13fde9ccb463de5d85f9ab3d89e179139099
Stefan Bjarni Hjaltested <stefanh@Stefans-MacBook-Pro.local>
2018-09-25 14:01:21 +0000

    q5 a10 added

--- mimir/assingnment_10/q5.py
+++ mimir/assingnment_10/q5.py
@@ -1,16 +1,16 @@
 def input_vector(size):
     vector = []
     for i in range(1,size+1):
-        elements = float(input("Element no {}: ".format(i)))
-        vector.append(elements)
+        hlutur = float(input("Element no {}: ".format(i)))
+        vector.append(hlutur)
     return vector
-def dot_product(vector1, vector2):
-    summa = 0
-    for i in range(0,len(vector1)):
-        summa += vector1[i] * vector2[i] 
-    return summa
 
-# Main program starts here, DO NOT change
+def dot_product(vector1, vector2):
+    sum1 = 0
+    for i in range(0,len(vector1)):
+        sum1 += vector1[i] * vector2[i] 
+    return sum1
+
 size = int(input("Input vector size: "))
 vector1 = input_vector(size)
 vector2 = input_vector(size)
